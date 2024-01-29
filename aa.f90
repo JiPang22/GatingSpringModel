127a8905e86f8e96c971dd75005d95a9b675b3a8
JiPang22 <wlvud6902@gmail.com>
2024-01-29 21:39:29 +0900

    add model detail

--- aa.f90
+++ aa.f90
@@ -1,29 +1,27 @@
 program aa
 implicit none
 integer i,imax
-real fhb,fmax,d,del,dt,ksp,kgs,lam,lama,gam,x,xa,y,ya,p0,ax,axa,ay,ydot,aydot,m,s,aya,t
-open(1,file='x') 
-fmax=50.;d=60.9
-del=1.;dt=1.e-2
-imax=5000;m=1.
-ksp=0.65;kgs=0.75
+real fhb,fmax,d,del,dt,ksp,kgs,lam,lama,gam,x,xa,y,p0,ax,axa,ay,m,s
+open(1,file='aa') 
+imax=5000;dt=1.e-2
 lam=0.1;lama=0.1;gam=0.1
-x=-100.;xa=-120.;y=1.;ya=1.;s=0.65
+fmax=52.;d=60.9;s=0.65;m=1.
+ksp=0.65;kgs=0.75;del=1.
+x=-145.;xa=-135.;y=1.
 
 do i=1,imax
-p0=1./(1.-exp(-(x-xa)))
-fhb=-gam*y-kgs*(x-xa-d*p0)
-aydot=-gam*y+fhb/m
-ay=ydot
+p0=1./(1.+exp(-(x-xa)))!probability
+fhb=-gam*y-kgs*(x-xa-d*p0)-ksp*x!applied force
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ay=-gam*y+fhb/m
 ax=y
-aya=(kgs/lama)*(x-xa-d*p0)-(fmax/lama)*(1.-s*p0)
-axa=ya
-ydot=ydot+aydot*dt
-y=y+ay*dt
-x=x+ax*dt
-xa=xa+axa*dt
-ya=ya+aya*dt
-t=i*dt
-write(1,*) x,y
+
+axa=(kgs/lama)*(x-xa-d*p0)-(fmax/lama)*(1.-s*p0)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+y=y+ay*dt!velocity of x
+x=x+ax*dt!position of hair bundle
+xa=xa+axa*dt!position of motor protein
+!write(1,*) i*dt,x
+write(1,*) x,xa
 enddo
 end
